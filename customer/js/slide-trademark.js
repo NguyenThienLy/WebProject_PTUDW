921c5266bfa0e29cba59b43a07281bed2cae40b4
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-05-03 00:32:29 +0700

    update responsive customer

---
+++ customer/js/slide-trademark.js
@@ -0,0 +1,43 @@
+$(".responsive").slick({
+  dots: true,
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 922,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
