d26397fc3791d86f50b04bf4cc2ce00706bbff6c
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-05-03 16:20:44 +0700

    update responsive customer

--- customer/js/carousel-margin.js
+++ customer/js/carousel-margin.js
@@ -1,12 +1,12 @@
 // carousel margin top = height of header
 function update_margin_top_header_carousel() {
   var win = $(this); //this = window
-  if (win.outerWidth() <= 992) {
+  // if (win.outerWidth() <= 991) {
     var result = $(".component-header").height();
-    $(".header-carousel").css("margin-top", result + 16);
-  } else {
-    $(".header-carousel").css("margin-top", 0);
-  }
+    $(".header-carousel").css("margin-top", result);
+  // } else {
+  //   $(".header-carousel").css("margin-top", 0);
+  // }
 }
 $(window).on("resize", update_margin_top_header_carousel);
 
--- customer/js/user-account-modal.js
+++ customer/js/user-account-modal.js
@@ -1,10 +1,10 @@
 $('p[data-toggle="tab"]').on('show.bs.tab', function (e) {
     var target = $(e.target).attr("href") // activated tab
-    if(target === "#header-login-modal-nav") {
-        document.getElementById("header-user-modal-title").innerText = "Đăng nhập";
-        document.getElementById("header-user-modal-content").innerText = "Đăng nhập để theo dõi đơn hàng, lưu danh sách sản phẩm yêu thích, nhận nhiều ưu đãi hấp dẫn.";
-    } else if (target === "#header-register-modal-nav") {
-        document.getElementById("header-user-modal-title").innerText = "Đăng ký";
-        document.getElementById("header-user-modal-content").innerText = "Tạo tài khoản để theo dõi đơn hàng, lưu danh sách sản phẩm yêu thích, nhận nhiều ưu đãi hấp dẫn.";
+    if(target === "#header-info-modal-nav") {
+        document.getElementById("header-avatar-modal-title").innerText = "Thông tin tài khoản";
+        document.getElementById("header-avatar-modal-content").innerText = "Quí khách cần đảm bảo cung cấp thông tin một cách chính xác để chúng tôi có thể liên hệ và tương tác một cách tốt nhất.";
+    } else if (target === "#header-edit-info-modal-nav") {
+        document.getElementById("header-avatar-modal-title").innerText = "Chỉnh sửa thông tin";
+        document.getElementById("header-avatar-modal-content").innerText = "Luôn cập nhật thông tin cá nhân để có trải nghiệm tốt.";
     }
 });
