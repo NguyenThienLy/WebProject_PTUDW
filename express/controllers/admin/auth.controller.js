baf256f9500157df07ec85f93bf372742b354aa6
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-05-14 02:19:02 +0700

    update login logout (fix database)

--- express/controllers/admin/auth.controller.js
+++ express/controllers/admin/auth.controller.js
@@ -4,21 +4,21 @@ var adminModel = require("../../models/admin.model");
  
 module.exports.login = function(req, res) {
   res.render("admin/login-admin");
- 
+
   // Xóa giá trị cookie khi logout
   res.cookie("adminId", "");
 };
  
 module.exports.postLogin = function(req, res, next) {
   try {
-    var username = req.body.NAME;
+    var username = req.body.USERNAME;
     var password = req.body.PASSWORD;
  
     // Đăng kí tài khoản với mật khẩu mã hóa bằng bcrypt
     // bcrypt.hash(password, saltRounds, function(err, hash) {
     // 	// Store hash in your password DB.
     // 	var entity = {
-    // 		NAME: username,
+    // 		USERNAME: username,
     // 		PASSWORD: hash
     // 	}
  
@@ -39,41 +39,33 @@ module.exports.postLogin = function(req, res, next) {
     admins
       .then(rows => {
         var admin = rows.find((value, index, array) => {
-          if (value.NAME == username) {
+          if (value.USERNAME == username) {
             return value;
           } else {
             return null;
           }
         });
 
-        // // Nếu không tìm thấy acc admin trùng khớp
-        // if (!admin) {
-        //   res.render("admin/login-admin", {
-        //     errors: [{ error: "Admin does not exist!" }],
-        //     values: req.body
-        //   });
-        //   return;
-        // }
+        // Nếu không tìm thấy acc admin trùng khớp
+        if (!admin) {
+          res.render("admin/login-admin", {
+            errors: [{ error: "Admin does not exist!" }],
+            values: req.body
+          });
+          return;
+        }
       
         // // Kiểm tra mật khẩu
-        // var hash = admin.PASSWORD;
-        // console.log(password);
-        // console.log(hash);
-        // bcrypt.compare(password, hash, function(err, res_bcrypt) {
-        //   if (res_bcrypt == false) {
-        //     res.render("admin/login-admin", {
-        //       errors: [{ error: "Wrong password!" }],
-        //       values: req.body
-        //     });
-        //     return;
-        //   }
-        //   // Thiết lập giá trị cookie = admin.ID
-        //   res.cookie("adminId", admin.ID, {
-        //     signed: true
-        //   });
-        //   // Chuyển đến trang index
-        //   res.redirect("/admin/index");
-        // });
+        var hash = admin.PASSWORD;
+        bcrypt.compare(password, hash, function(err, res_bcrypt) {
+          if (res_bcrypt == false) {
+            res.render("admin/login-admin", {
+              errors: [{ error: "Wrong password!" }],
+              values: req.body
+            });
+            return;
+          }
+        });
 
         // Thiết lập giá trị cookie = admin.ID
         res.cookie("adminId", admin.ID, {
@@ -86,6 +78,7 @@ module.exports.postLogin = function(req, res, next) {
       .catch(err => {
         console.log(err);
       });
+
   } catch (error) {
     next(error);
   }
