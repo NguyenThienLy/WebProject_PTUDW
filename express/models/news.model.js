82f7d1f182cb90fe2587e02997615cdb2c516ed4
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-05-15 23:17:28 +0700

    update project

--- express/models/news.model.js
+++ express/models/news.model.js
@@ -3,8 +3,8 @@ var db = require("../utils/db");
 
 // Hàm trả về 8 sản phẩm sale nhiều nhất trong index customer
 module.exports.top3PopularNewsIndexForIndex = () => {
-  return db.load(`SELECT ID, IMAGE, TITLE, SHORTCONENT, DATE(CREATED) AS CREATED					
+  return db.load(`SELECT ID, IMAGE, TITLE, SHORTCONENT, CREATED				
 									FROM news
-                                    ORDER BY CREATED DESC
-                                    LIMIT 3;`);
+                  ORDER BY CREATED DESC
+                  LIMIT 3;`);
 };
