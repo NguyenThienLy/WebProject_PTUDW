e233775138ad99510f344a8625347b518da61342
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-05-17 20:11:36 +0700

    category

--- express/middlewares/customer/category.middleware.js
+++ express/middlewares/customer/category.middleware.js
@@ -1,14 +1,42 @@
 // Gọi categoryModel
-var categoryModel = require("../../models/category.model")
+var categoryModel = require("../../models/category.model");
+// Gọi subCategoryModel
+var subCategoryModel = require("../../models/sub_category.model");
 
 // Load lên các giá trị của catogory và sub category
 module.exports = (req, res, next) => {
-    var dataCategories = categoryModel.allWithDetailQuantity();
-    
-    dataCategories.then(rows => {
-        res.locals.lcCategories = rows;
-        
-        console.log("haha", rows);
-        next();
-    })
-}
+  var dataCategories = categoryModel.allWithDetailQuantity();
+  var dataSubCategories = subCategoryModel.allSubCategoryWithDetailQuantity(5);
+
+  //   subCategoryModel.allSubCategoryWithDetailQuantity(5).then(subCategories => {
+  //     console.log(subCategories);
+  //   });
+
+  //   dataCategories.then(categories => {
+  //     //   console.log("categories",categories[]);
+  //     //   console.log("categories",categories);
+  //     subCategoryModel.allSubCategoryWithDetailQuantity(5).then(subCategories => {
+  //       res.locals.lcCategories = categories;
+  //       res.locals.lcSubCategories = subCategories;
+
+  //       console.log(categories);
+  //       console.log(subCategories);
+
+  //       next();
+  //     });
+  //   });
+
+  dataCategories.then(categories => {
+    //   console.log("categories",categories[]);
+    //   console.log("categories",categories);
+    res.locals.lcCategories = categories;
+
+    next();
+  });
+};
+
+// Handlebars.registerHelper("ifIsNthItem", function() {
+//     //  var subCategories =;
+
+//     return  `<a class="dropdown-item" href="product-show.html">Rau cu 1</a>`;
+// });
