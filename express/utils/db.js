4cae69cbe34a03e5e6d77f09858359247b70857f
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-06-15 01:53:07 +0700

    fix

--- express/utils/db.js
+++ express/utils/db.js
@@ -14,7 +14,7 @@ var CreateConnection = () => {
     user: "root",
     port: "3306",
     password: "",
-    database: "cpqzk1zfm6"
+    database: "new_organic"
   });
 
   // return mysql.createConnection({
