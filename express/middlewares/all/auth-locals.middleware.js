e84a6159d30c38fa1fffe37e7e68d8931c6b813c
Qui <qui021098@gmail.com>
2019-06-15 08:34:29 +0700

    load info customer

--- express/middlewares/all/auth-locals.middleware.js
+++ express/middlewares/all/auth-locals.middleware.js
@@ -2,6 +2,8 @@ module.exports = (req, res, next) => {
     if (req.user) {
       res.locals.isAuthenticated = true;
       res.locals.authUser = req.user;
+
+      console.log('t da o day',res.locals.authUser);
     }
     next();
   }
