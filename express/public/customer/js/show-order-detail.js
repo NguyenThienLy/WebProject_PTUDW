da3f0e3390675542985b648e8e5fa603d2cc8a97
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-06-19 10:52:50 +0700

    paginate order-info-show customer

--- express/public/customer/js/show-order-detail.js
+++ express/public/customer/js/show-order-detail.js
@@ -1,18 +1,63 @@
 $(document).ready(function() {
+  var url = window.location.href;
+  var isStatus1 = url
+    .substring(url.lastIndexOf("/") + 1)
+    .indexOf("order-info-show-status-1");
+
+  var isStatus2 = url
+    .substring(url.lastIndexOf("/") + 1)
+    .indexOf("order-info-show-status-2");
+
+  var isStatus3 = url
+    .substring(url.lastIndexOf("/") + 1)
+    .indexOf("order-info-show-status-3");
+
+  var isStatus4 = url
+    .substring(url.lastIndexOf("/") + 1)
+    .indexOf("order-info-show-status-4");
+
+    if (isStatus1 === 0) {
+        $("#nav-order-info-type-1-tab").tab("show");
+        $("#collapse-order-type-1").collapse("show");
+        $("#collapse-order-type-2").collapse("hide");
+        $("#collapse-order-type-3").collapse("hide");
+        $("#collapse-order-type-4").collapse("hide");
+    }
+    if (isStatus2 === 0) {
+        $("#nav-order-info-type-2-tab").tab("show");
+        $("#collapse-order-type-2").collapse("show");
+        $("#collapse-order-type-1").collapse("hide");
+        $("#collapse-order-type-3").collapse("hide");
+        $("#collapse-order-type-4").collapse("hide");
+    }
+    if (isStatus3 === 0) {
+        $("#nav-order-info-type-3-tab").tab("show");
+        $("#collapse-order-type-3").collapse("show");
+        $("#collapse-order-type-2").collapse("hide");
+        $("#collapse-order-type-1").collapse("hide");
+        $("#collapse-order-type-4").collapse("hide");
+    }
+    if (isStatus4 === 0) {
+        $("#nav-order-info-type-4-tab").tab("show");
+        $("#collapse-order-type-4").collapse("show");
+        $("#collapse-order-type-2").collapse("hide");
+        $("#collapse-order-type-3").collapse("hide");
+        $("#collapse-order-type-1").collapse("hide");
+    }
+    
+
   // Tăng số lượng sản phẩm trong giỏ hàng product simple
   $(document).on("click", "#btnShowDetail", function() {
     var $id = $(this).attr("id-show-detail");
 
-      $.post(
-        "/customer/order/order-detail-show",
-        { orderInfoId: $id },
-        function(data) {
-            emptyContain();
-            showOrderDetailProductSimple(JSON.parse(data.productsSimple));
-            showOrderDetailProductCombo(JSON.parse(data.productsCombo));
-            $("#orderDetailModal").modal();
-        }
-      );
+    $.post("/customer/order/order-detail-show", { orderInfoId: $id }, function(
+      data
+    ) {
+      emptyContain();
+      showOrderDetailProductSimple(JSON.parse(data.productsSimple));
+      showOrderDetailProductCombo(JSON.parse(data.productsCombo));
+      $("#orderDetailModal").modal();
+    });
   });
 
   function emptyContain() {
@@ -33,28 +78,38 @@ $(document).ready(function() {
   }
 
   function appendProductSimple(productSimple) {
-      //alert(productSimple);
+    //alert(productSimple);
     $("#orderDetailModal #containProductSimpleInOrderDetailShow").append(
       `
       <div class="row mt-2 mr-2 ml-2">
           <div class="component-cart-quantity card d-flex flex-row align-items-center p-4 w-100 h-100">
 
-              <div class="position-relative h-100 content-left" data-toggle="tooltip" title="${productSimple.NAME}">
+              <div class="position-relative h-100 content-left" data-toggle="tooltip" title="${
+                productSimple.NAME
+              }">
                   <a class="stretched-link"
-                      href="/customer/product/product-detail/product-simple/${productSimple.ID}"></a>
+                      href="/customer/product/product-detail/product-simple/${
+                        productSimple.ID
+                      }"></a>
                   <img src="${productSimple.IMAGE}" class="h-100 w-100" alt="">
               </div>
 
               <div class="d-flex content-right">
                   <div class="product-info d-flex align-items-center">
-                      <a href="/customer/product/product-detail/product-simple/${productSimple.ID}"
-                          class="title product-name"><strong>${productSimple.FORMATNAME}</strong></a>
+                      <a href="/customer/product/product-detail/product-simple/${
+                        productSimple.ID
+                      }"
+                          class="title product-name"><strong>${
+                            productSimple.FORMATNAME
+                          }</strong></a>
                   </div>
 
                   <div class="d-flex align-items-center justify-content-center product-quantity">
 
                       <input type="number" id="inputQuantityProduct" class="mr-1 text-center p-1"
-                          id-product-cart-detail="productSimple${productSimple.ID}" value="${productSimple.QUANTITY}" />
+                          id-product-cart-detail="productSimple${
+                            productSimple.ID
+                          }" value="${productSimple.QUANTITY}" />
 
                   </div>
 
@@ -65,7 +120,9 @@ $(document).ready(function() {
 
                       <div class="mt-2 font-weight-bold total-price">
                           <span id="spanSumPriceInRow"
-                              id-product-cart-detail="productSimple${productSimple.ID}">${productSimple.TOTALMONEY}</span>
+                              id-product-cart-detail="productSimple${
+                                productSimple.ID
+                              }">${productSimple.TOTALMONEY}</span>
                           <span><u>đ</u></span>
                       </div>
                   </div>
@@ -85,22 +142,36 @@ $(document).ready(function() {
               <div class="position-relative h-100 content-left d-flex align-items-center"
                   data-toggle="tooltip" title="${productCombo.NAME}">
                   <a class="stretched-link"
-                      href="/customer/product/product-detail/product-simple/${productCombo.ID}"></a>
-                  <img src="${productCombo.IMAGE1}" style="width: 33.33%;" alt="" />
-                  <img src="${productCombo.IMAGE2}" style="width: 33.33%;" alt="" />
-                  <img src="${productCombo.IMAGE3}" style="width: 33.33%;" alt="" />
+                      href="/customer/product/product-detail/product-simple/${
+                        productCombo.ID
+                      }"></a>
+                  <img src="${
+                    productCombo.IMAGE1
+                  }" style="width: 33.33%;" alt="" />
+                  <img src="${
+                    productCombo.IMAGE2
+                  }" style="width: 33.33%;" alt="" />
+                  <img src="${
+                    productCombo.IMAGE3
+                  }" style="width: 33.33%;" alt="" />
               </div>
 
               <div class="d-flex content-right">
                   <div class="product-info  d-flex align-items-center" >
-                      <a href="/customer/product/product-detail/product-simple/${productCombo.NAME}"
-                          class="title product-name"><strong>${productCombo.FORMATNAME}</strong></a>
+                      <a href="/customer/product/product-detail/product-simple/${
+                        productCombo.NAME
+                      }"
+                          class="title product-name"><strong>${
+                            productCombo.FORMATNAME
+                          }</strong></a>
                   </div>
 
                   <div class="d-flex align-items-center justify-content-center product-quantity">
 
                       <input type="number" id="inputQuantityProduct" class="mr-1 text-center p-1"
-                          id-product-cart-detail="productCombo${productCombo.NAME}" value="${productCombo.QUANTITY}"
+                          id-product-cart-detail="productCombo${
+                            productCombo.NAME
+                          }" value="${productCombo.QUANTITY}"
                           disabled="false" />
                   </div>
 
@@ -111,7 +182,9 @@ $(document).ready(function() {
 
                       <div class="mt-2 font-weight-bold total-price">
                           <span id="spanSumPriceInRow"
-                              id-product-cart-detail="productCombo${productCombo.NAME}">${productCombo.TOTALMONEY}</span>
+                              id-product-cart-detail="productCombo${
+                                productCombo.NAME
+                              }">${productCombo.TOTALMONEY}</span>
                           <span><u>đ</u></span>
                       </div>
 
@@ -123,3 +196,24 @@ $(document).ready(function() {
     );
   }
 });
+
+// $(function() {
+//     var url = window.location.href;
+//     var isStatus1 = url
+//       .substring(url.lastIndexOf("/") + 1)
+//       .indexOf("order-info-show-status-1");
+
+//       var isStatus2 = url
+//       .substring(url.lastIndexOf("/") + 1)
+//       .indexOf("order-info-show-status-2");
+
+//       var isStatus3 = url
+//       .substring(url.lastIndexOf("/") + 1)
+//       .indexOf("order-info-show-status-3");
+
+//       var isStatus4 = url
+//       .substring(url.lastIndexOf("/") + 1)
+//       .indexOf("order-info-show-status-4");
+
+//     return isSimple;
+// }
